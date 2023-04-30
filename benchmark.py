cd4b8eecb424ead5adf15c352b8d0082c2656a4c
Erik van Zijst <erik.van.zijst@gmail.com>
2023-04-30 22:07:34 +0200

    Add more data points

--- benchmark.py
+++ benchmark.py
@@ -3,7 +3,7 @@
 import subprocess
 
 if __name__ == '__main__':
-    for prog in ['mproc2', 'mthreading', 'async', 'sequential']:
-        for concurrency in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 20, 24, 28, 32, 36, 40, 44, 48, 64]:
+    for concurrency in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 20, 24, 28, 32, 36, 40, 44, 48, 64]:
+        for prog in ['mproc2', 'mthreading', 'async', 'sequential']:
             print(f'Running {prog} with concurrency {concurrency}...')
             subprocess.check_call(['python3', 'crawl.py', '-p', prog, '-c', str(concurrency)])
